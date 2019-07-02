ec9eba3690f09abef0cbe7b013d3d43bf2bcab8c
zhangcheng liang <zlian030@wch136-21.cs.ucr.edu>
2019-07-02 01:54:47 -0700

    part 3

--- source/main.c
+++ source/main.c
@@ -14,7 +14,7 @@
 #endif
 
 enum States {start, ADD, ADDwait, SUB, SUBwait, wait, reset} state;
-unsigned char temp = 0x00;
+unsigned char temp = 0x01;
 void Tick();
 
 int main(void) {
@@ -32,85 +32,100 @@ DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize t
 void Tick() {
     switch(state) {
 	case start:
-	    state = wait;
+	    state = waitRelease;
 	    break;
-	case ADD:
-	    state = ADDwait;
-	    break;
-	case ADDwait:
-	    if ((PINA & 0x01) && (PINA & 0x02)) {
-		state = reset;
+        case seq1:
+	    if (PINA == 0x01) {
+		temp = 0x02;
+                state = waitRelease;
 	    }
-	    else if (!(PINA & 0x01) && (PINA & 0x02)) {
-		state = SUB;
-	    }
-	    else if (!(PINA & 0x01) && !(PINA & 0x02)) {
-		state = wait;
-	    }
-	    else {
-		state = ADDwait;
+            break;
+	case seq2:
+	    if (PINA == 0x01) {
+		temp = 0x03;
+                state = waitRelease;
 	    }
 	    break;
-	case SUB:
-	    state = SUBwait;
-	    break;
-	case SUBwait:
-            if ((PINA & 0x01) && (PINA & 0x02)) {
-                state = reset;
-            }
-            else if (!(PINA & 0x02) && (PINA & 0x01)) {
-                state = ADD;
-            }
-	    else if (!(PINA & 0x01) && !(PINA & 0x02)){
-		state = wait;
+	case seq3;
+	    if (PINA == 0x01) {
+		temp = 0x04;
+                state = waitRelease;
 	    }
-            else {
-                state = SUBwait;
+	    break;
+        case seq4:
+            if (PINA == 0x01) {
+                temp = 0x05;
+                state = waitRelease;
             }
             break;
-        case wait:
-            if ((PINA & 0x01) && (PINA & 0x02)) {
-                state = reset;
+        case seq5:
+            if (PINA == 0x01) {
+                temp = 0x06;
+                state = waitRelease;
             }
-            else if (!(PINA & 0x02) && (PINA & 0x01)) {
-                state = ADD;
-            }
-	    else if (!(PINA & 0x01) && (PINA & 0x02)) {
-                state = SUB;
-            }
-	    else {
-		state = wait;
-	    } 
             break;
-	case reset:
-	    state = wait;
-	    break;
-	default:
-	    state = wait;
-	    break;
-    }
+        case seq6:
+            if (PINA == 0x01) {
+                temp = 0x01;
+		state = waitRelease;
+            }
+            break;
 
-    switch(state) {
-        case start:
-            break;
-        case ADD:
-            temp = (temp == 0x09) ? 0x09 : (temp + 0x01);
-            break;
-	case ADDwait:
-	    break;
-        case SUB:
-	    temp = (temp == 0x00) ? 0x00 : (temp - 0x01);
-            break;
-	case SUBwait:
-	    break;
-        case wait:
-            break;
-	case reset:
-	    temp = 0x00;
+
+	case waitRelease:
+	    if (PINA == 0x00) {
+		if (temp == 0x01) {
+		    state = seq1;
+		}
+                else if (temp == 0x02) {
+                    state = seq2;
+                }
+                else if (temp == 0x03) {
+                    state = seq3;
+                }
+                else if (temp == 0x04) {
+                    state = seq4;
+                }
+                else if (temp == 0x05) {
+                    state = seq5;
+                }
+		else {
+		    state = seq6;
+		}
+	    }
 	    break;
         default:
+	    state = waitRelease;
             break;
     }
+
+    switch (state) {
+	case start:
+	    break;
+        case seq1:
+	    PORTB = 0x03;
+	    break;
+        case seq2:
+            PORTB = 0x06;
+            break;
+        case seq3:
+            PORTB = 0x0C;
+            break;
+        case seq4:
+            PORTB = 0x18;
+            break;
+        case seq5:
+            PORTB = 0x30;
+            break;
+        case seq6:
+            PORTB = 0x60;
+            break;
+
+
+
+    } 
     
+
+
     PORTC = temp;    
 }
