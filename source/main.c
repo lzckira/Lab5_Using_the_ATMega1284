11aaf03d42d44893bc020e869e0d3f03db4f9895
zhangcheng liang <zlian030@wch136-26.cs.ucr.edu>
2019-07-02 15:12:38 -0700

    part 3

--- source/main.c
+++ source/main.c
@@ -13,15 +13,14 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States {start, press, waitRelease} state;
-//unsigned char order = 0x01;
-unsigned char cont = 0x00;
-
+enum States {start, ADD, ADDwait, SUB, SUBwait, wait, reset} state;
+unsigned char temp = 0x00;
+unsigned char temp2 = 0x00;
 void Tick();
 
 int main(void) {
 
-DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+DDRA = 0x00; PORTA = 0xFF; // Configure port B's 8 pins as inputs
 DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
     state = start;
     while (1) {
@@ -34,101 +33,73 @@ DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize t
 void Tick() {
     switch(state) {
 	case start:
-	    state = waitRelease;
+	    state = wait;
 	    break;
-	case press:
-	    if (~PINA == 0x00) {
-	        state = waitRelease;
-	        cont = cont + 0x01;
+	case ADD:
+	    state = ADDwait;
+	    break;
+	case ADDwait:
+	    if (!(~PINA & 0x01)) {
+		state = wait;
 	    }
 	    else {
-		state = press;
+		state = ADDwait;
 	    }
 	    break;
-	
-	case waitRelease:
-	    if (~PINA == 0x00) {
-		state = press;
-	    }
-	    else {
-		state = waitRelease;
-	    }
+	case SUB:
+	    state = SUBwait;
 	    break;
-        default:
-	    state = waitRelease;
+	case SUBwait:
+	    if (!(~PINA & 0x01)){
+		state = wait;
+	    }
+            else {
+                state = SUBwait;
+            }
             break;
+        case wait:
+            if ((~PINA & 0X01) && (~temp2)) {
+                state = ADD;
+            }
+	    else if ((~PINA & 0x01)  && (temp2)) {
+                state = SUB;
+            }
+	    else {
+		state = wait;
+	    } 
+            break;
+	case reset:
+	    state = wait;
+	    break;
+	default:
+	    state = wait;
+	    break;
     }
 
-    switch (state) {
-	case start:
+    switch(state) {
+        case start:
+            break;
+        case ADD:
+            temp = temp + 0x01;
+if (temp == 7) {temp2 = 1;}
+            break;
+	case ADDwait:
 	    break;
-        case press:
-	    if (cont == 0x00) {
-		 PORTC = 0x01;   
-	    }
-	    else if (cont == 0x01) {
-                 PORTC = 0x02;
-	    
-	    }
-	    else if (cont == 0x02) {
-                 PORTC = 0x04;
-	    
-	    }
-            else if (cont == 0x03) {
-                 PORTC = 0x08;
-	    
-	    }
-            else if (cont == 0x04) {
-                 PORTC = 0x10;
-	    
-	    }
-            else if (cont == 0x05) {
-                 PORTC = 0x20;
-	    
-	    }
-            else {
-                 PORTC = 0x00;
-	    
-	    }
-	    break;    
-        case waitRelease:
-            if (cont == 0x00) {
-                 PORTC = 0x01;
-            }
-            else if (cont == 0x01) {
-                 PORTC = 0x02;
+        case SUB:
+	    temp = temp - 0x01;
 
-            }
-            else if (cont == 0x02) {
-                 PORTC = 0x03;
-
-            }
-            else if (cont == 0x03) {
-                 PORTC = 0x04;
-
-            }
-            else if (cont == 0x04) {
-                 PORTC = 0x05;
-
-            }
-            else if (cont == 0x05) {
-                 PORTC = 0x06;
-
-            }
-            else {
-                 PORTC = 0x00;
-
-            }
-
+if (temp == 0) {temp2 = 0;}
             break;
+	case SUBwait:
+	    break;
+        case wait:
+            break;
+	case reset:
+//	    temp = 0x00;
+	    break;
         default:
-
             break;
-
-
-    } 
+    }
     
-
-
-      
+    PORTC = 0x01 << temp;    
 }
